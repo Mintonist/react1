8c8cc7ec27ce0cab409873946f9a0d8f3c9fe9a6
kevin <kevin@kevin.tt>
2023-03-14 18:53:10 +0300

    КП. Модуль 2. Frontend 3. Композиция компонентов

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,46 +1,66 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Counter = () => {
-  //--> почему нельзя вызывать setCount внутри этой функции, а только асинхронно?
-  //--> почему нельзя не использовать setCount - иначе ошибка
-  //--> почему нельзя не использовать импортируемые методы?
-  let [count, setCount] = useState(0);
-  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
-  const formatCount = () => (count == 0 ? 'Empty' : count);
+interface CounterProps {
+  id: number;
+  value: number;
+  name: string;
+  onDelete: any;
+  onIncrement: any;
+  onDecrement: any;
+}
+
+//--> почему нельзя вызывать setCount внутри этой функции, а только асинхронно?
+//--> почему нельзя не использовать setCount - иначе ошибка
+//--> почему нельзя не использовать импортируемые методы?
+//-> где происходит связь типов из models.ts и объектов из API?
+const Counter = (props: CounterProps) => {
+  //let [value, setValue] = useState(props.value);
+  let value: number = props.value;
+
+  const formatValue = () => (value == 0 ? 'Empty' : value);
   const getBageClasses = () =>
-    'badge m-2 ' + (count != 0 ? 'bg-primary' : 'bg-warning');
+    'badge p-2 m-2 ' + (value != 0 ? 'bg-primary' : 'bg-warning');
 
-  const handleIncrement = () => setCount(count + 1);
-  const handleDecrement = () => setCount(count - 1);
-  const handleTagChange = (id: string) =>
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-
-  const renderTags = () => {
-    return (
-      tags.length !== 0 &&
-      tags.map((tag) => (
-        <li
-          key={tag}
-          className="btn btn-primary btn-sm m-2"
-          onClick={() => handleTagChange(tag)}
-        >
-          {tag}
-        </li>
-      ))
-    );
-  };
+  // const handleIncrement = () => {
+  //   value++;
+  // }; //setValue(value + 1);
+  // const handleDecrement = () => {
+  //   if (value > 0) {
+  //     value--;
+  //     //setValue(value - 1);
+  //   }
+  // };
 
   return (
-    <>
-      <ul>{renderTags()}</ul>
-      <span className={getBageClasses()}>{formatCount()}</span>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+    <div>
+      {/* <ul>{renderTags()}</ul> */}
+      <span>{props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => {
+          props.onIncrement(props.id);
+        }}
+      >
         +
       </button>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => {
+          props.onDecrement(props.id);
+        }}
+      >
         -
       </button>
-    </>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => {
+          props.onDelete(props.id);
+        }}
+      >
+        Delete
+      </button>
+    </div>
   );
 };
 
